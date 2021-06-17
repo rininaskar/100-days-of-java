3c5997f458c0b4fe7d3199cf236ef53b7d79651a
Rini Naskar <rininaskar1999@gmail.com>
2021-06-17 22:59:08 +0530

    Created Division Of Two Numbers

---
+++ src/java/DivideTwoNumbers.java
@@ -0,0 +1,11 @@
+public class DivideTwoNumbers {
+
+    public static void main(String[] args) {
+
+        double var1 = 10;
+        double var2 = 5;
+        double var3 = var1/var2;
+
+        System.out.println("Division Of Two Numbers : " +var3);
+    }
+}
