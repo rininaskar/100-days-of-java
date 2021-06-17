689f8faa1b10e5f04aea39e46a940ba98730c7e7
Rini Naskar <rininaskar1999@gmail.com>
2021-06-17 22:52:28 +0530

    Created Multiplication Of Two Numbers

---
+++ src/java/MultiplicationOfTwoNumbers.java
@@ -0,0 +1,11 @@
+public class MultiplicationOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int var1 = 2;
+        int var2 = 3;
+        int var3 = var1 * var2;
+
+        System.out.println("Multiplication of two numbers :" +var3);
+    }
+}
