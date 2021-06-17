c2ef8fee5436bef39958a5bed4953791c8575957
Rini Naskar <rininaskar1999@gmail.com>
2021-06-17 22:37:13 +0530

    Created Sum Of Two Numbers

---
+++ src/java/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        //assign two values into two variables and calculate the sum in the third variable
+        int var1 = 4;
+        int var2 = 6;
+        int var3 = var1 + var2;
+
+        System.out.println("Sum of two numbers is : " +var3);
+
+
+    }
+}
