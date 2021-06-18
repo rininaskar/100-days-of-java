ecd185b6057916396213581af7d3cb4c8d9878c3
Rini Naskar <rininaskar1999@gmail.com>
2021-06-18 15:34:57 +0530

    Created Subraction Of Two Numbers

---
+++ src/java/SubtractionOfTwoNumbers.java
@@ -0,0 +1,11 @@
+public class SubtractionOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int var1 = 20;
+        int var2 = 5;
+        int var3 = var1 - var2;
+
+        System.out.println("Subtraction Of Two Numbers : "+var3);
+    }
+}
