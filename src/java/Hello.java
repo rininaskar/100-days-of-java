a2b648ea16163cac84cae21d374d631945d61b8c
Rini Naskar <rininaskar1999@gmail.com>
2021-06-18 15:46:41 +0530

    Created Hello Rini

--- src/java/Hello.java
+++ src/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello Rini");
+
+        int myFirstNumber = 8;
+        System.out.println(myFirstNumber);
     }
 }
