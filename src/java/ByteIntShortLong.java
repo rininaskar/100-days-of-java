8b3351353b9a0494dd22de84e6bd412a5a85e8a0
Rini Naskar <rininaskar1999@gmail.com>
2021-06-19 16:24:49 +0530

    Created ByteIntShortLong

---
+++ src/java/ByteIntShortLong.java
@@ -0,0 +1,16 @@
+public class ByteIntShortLong {
+
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println(" Integer Minium Value = " +myMinIntValue);
+        System.out.println(" Integer Maximum Value = " +myMaxIntValue);
+
+
+    }
+}
