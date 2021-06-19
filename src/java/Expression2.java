9989e27a705b10bf4788765245a53a4d371aa9c2
Rini Naskar <rininaskar1999@gmail.com>
2021-06-19 16:05:13 +0530

    Created Expressions with three variables with twist

---
+++ src/java/Expression2.java
@@ -0,0 +1,14 @@
+public class Expression2 {
+
+    public static void main(String[] args) {
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println( myTotal );
+    }
+}
+
+
