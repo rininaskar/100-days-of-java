8ee68477564164508f850aef6e785bd6dc7c703b
Rini Naskar <rininaskar1999@gmail.com>
2021-06-19 15:12:13 +0530

    Created Hello2

---
+++ src/java/Hello2.java
@@ -0,0 +1,11 @@
+public class Hello2 {
+
+    public static void main(String[] args) {
+
+        int myFirstNumber = 8;
+        int mySecondNumber = 2;
+
+
+        System.out.println(mySecondNumber);
+    }
+}
---
+++ src/java/Hello3.java
@@ -0,0 +1,12 @@
+public class Hello3 {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 10;
+        int mySecondNumber = 30;
+        int myThirdNumber = myFirstNumber + mySecondNumber;
+
+        System.out.println( +myThirdNumber );
+    }
+
+
+}
