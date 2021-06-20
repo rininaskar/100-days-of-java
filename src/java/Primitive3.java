408d484b14f432ebbf0d875a14b8320f91d79c88
Rini Naskar <rininaskar1999@gmail.com>
2021-06-20 16:36:11 +0530

    Created Primitive  With Float

---
+++ src/java/Primitive3.java
@@ -0,0 +1,10 @@
+public class Primitive3 {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println( "Float minimum value = " + myMinFloatValue);
+        System.out.println( "Float maximum value = " + myMaxFloatValue);
+    }
+}
