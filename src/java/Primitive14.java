881f0d9b4c7be57d7005207a8eafdd5996938fa9
Rini Naskar <rininaskar1999@gmail.com>
2021-06-21 12:25:07 +0530

    Created Primitive Types Char

---
+++ src/java/Primitive14.java
@@ -0,0 +1,10 @@
+public class Primitive14 {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println( myChar );
+        System.out.println( myUnicodeChar );
+    }
+}
