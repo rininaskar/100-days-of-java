ce482fd31025c9d2518b5384e98937066823ece1
Rini Naskar <rininaskar1999@gmail.com>
2021-06-21 11:45:26 +0530

    Created Primitive Types With Value

---
+++ src/java/Primitive6.java
@@ -0,0 +1,23 @@
+public class Primitive6 {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println( "Float minimum value = " + myMinFloatValue);
+        System.out.println( "Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println( "Double minimum value = " + myMinDoubleValue);
+        System.out.println( "Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+        System.out.println( " MyIntValue= " + myIntValue );
+        System.out.println( "MyFloatValue= " + myFloatValue);
+        System.out.println( "MyDoubleValue= " + myDoubleValue);
+    }
+}
