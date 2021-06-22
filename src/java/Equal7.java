910090fe8d532dc591927c4a78b4e8407115c472
Rini Naskar <rininaskar1999@gmail.com>
2021-06-22 12:45:32 +0530

    Created Operator Multiplying

---
+++ src/java/Equal7.java
@@ -0,0 +1,16 @@
+public class Equal7 {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; //1 + 2 = 3
+        System.out.println( "1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result -1;  //3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+        result = result * 10;  //2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+    }
+}
