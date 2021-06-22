f4a67c94da7e1edb0040474da169cdde2f0b249c
Rini Naskar <rininaskar1999@gmail.com>
2021-06-22 12:32:32 +0530

    Created Operator

---
+++ src/java/Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; //1 + 2 = 3
+        System.out.println( "1 + 2 = " + result);
+
+
+    }
+}
---
+++ src/java/Operator2.java
@@ -0,0 +1,10 @@
+public class Operator2 {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; //1 + 2 = 3
+        System.out.println( "1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+    }
+}
