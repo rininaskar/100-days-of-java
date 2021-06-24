ad94b73ee91a338dd58b1d1bccd5595c784eb557
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:17:53 +0530

    Created Logical Operators

---
+++ src/java/Logicals5.java
@@ -0,0 +1,10 @@
+public class Logicals5 {
+
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+    }
+}
