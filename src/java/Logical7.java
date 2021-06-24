1feef943ac2449366302e210b0d1ec53eb9d2bd4
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:26:03 +0530

    Created Logical Conditions

---
+++ src/java/Logical7.java
@@ -0,0 +1,15 @@
+public class Logical7 {
+
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100 ) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+    }
+}
