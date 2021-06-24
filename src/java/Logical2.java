5ea5c81db56db5b79e447b0e98915e0531ee501e
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:11:16 +0530

    Created Logicals

---
+++ src/java/Logical2.java
@@ -0,0 +1,10 @@
+public class Logical2 {
+
+    public static void main(String[] args) {
+
+        int topScore = 100;
+        if (topScore >= 100) {
+            System.out.println("You got the high score!");
+        }
+    }
+}
