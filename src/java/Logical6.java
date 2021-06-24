0afd13606e3fa9e3dc081f3081d3eb435740a860
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:24:08 +0530

    Created Logical Condition

---
+++ src/java/Logical6.java
@@ -0,0 +1,15 @@
+public class Logical6 {
+
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100 ) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+    }
+}
