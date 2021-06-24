e28137efe1c4b67b88b9f07581f3f09e9e2f2063
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:35:15 +0530

    Created Logical Conditions Check

---
+++ src/java/Logical9.java
@@ -0,0 +1,18 @@
+public class Logical9 {
+
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore && topScore < 100 )) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+    }
+}
