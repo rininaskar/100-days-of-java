5ad64139fa82254509b25bde8499f1939c5a47ec
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:05:39 +0530

    Created Logics

---
+++ src/java/Logic.java
@@ -0,0 +1,10 @@
+public class Logic {
+
+    public static void main(String[] args) {
+
+        int topScore = 100;
+        if (topScore != 100) {
+            System.out.println("You got the high score!");
+        }
+    }
+}
