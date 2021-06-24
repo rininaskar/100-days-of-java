2b7d11262905aaece5510bfde52dfb84dfeb1511
Rini Naskar <rininaskar1999@gmail.com>
2021-06-24 17:43:46 +0530

    Created Equal to Operator

---
+++ src/java/Logical10.java
@@ -0,0 +1,23 @@
+public class Logical10 {
+
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore && topScore < 100 )) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is an error");
+
+        }
+    }
+}
