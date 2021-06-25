8912afd8a9121ef1f5304516131878ecd69b8611
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:40:02 +0530

    Created Challenge

---
+++ src/java/Challenge.java
@@ -0,0 +1,10 @@
+public class Challenge {
+
+    public static void main(String[] args) {
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValueTotal = myFirstValue + mySecondValue * 100.00d;
+        System.out.println("MyValuesTotal = " + myValueTotal);
+    }
+}
