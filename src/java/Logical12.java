6fbb8a7412dedd421d3d15d45b9e59add7b7b147
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:19:47 +0530

    Created Equal to Operator

---
+++ src/java/Logical12.java
@@ -0,0 +1,25 @@
+public class Logical12 {
+    public static void main(String[] args) {
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore && topScore < 100 )) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (isCar = true) {
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
