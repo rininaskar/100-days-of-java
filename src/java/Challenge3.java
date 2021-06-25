e8d96ebf681fc1adbd47a70642554fa4646315be
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:46:12 +0530

    Created Challenge Ternary Operator

---
+++ src/java/Challenge3.java
@@ -0,0 +1,14 @@
+public class Challenge3 {
+
+    public static void main(String[] args) {
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNotRemainder = " + isNoRemainder);
+    }
+}
