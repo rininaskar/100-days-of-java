8b4c48950fa7b104dcdb989641874b93ee227251
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:27:19 +0530

    Created Equal to Operator Conditions False

---
+++ src/java/Logical16.java
@@ -0,0 +1,14 @@
+public class Logical16 {
+
+    public static void main(String[] args) {
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (isCar == false) {
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
