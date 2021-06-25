a1dcc8120483aed390d298d198c72a0291a9fb1e
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:34:24 +0530

    Created  Ternary Operator

---
+++ src/java/Logical19.java
@@ -0,0 +1,19 @@
+public class Logical19 {
+
+    public static void main(String[] args) {
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+    }
+}
