efdf1cd2845d3408a5f87c46e4bf56cbe3dbcfbb
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:42:48 +0530

    Created Challenge ValueTotal

---
+++ src/java/Challenge2.java
@@ -0,0 +1,12 @@
+public class Challenge2 {
+
+    public static void main(String[] args) {
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+    }
+}
