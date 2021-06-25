335c19e9134fbefa4dbc3e5381d3d7a45510cfbe
Rini Naskar <rininaskar1999@gmail.com>
2021-06-25 10:28:51 +0530

    Created Equal to Operator Conditions Not Operator

---
+++ src/java/Logical17.java
@@ -0,0 +1,14 @@
+public class Logical17 {
+
+    public static void main(String[] args) {
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
