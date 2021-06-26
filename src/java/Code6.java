3be4b2ecdfa4ca371eef7fe4c0e111595840a8da
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 13:14:19 +0530

    Expression Checking

---
+++ src/java/Code6.java
@@ -0,0 +1,13 @@
+public class Code6 {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 4000)
+            System.out.println("Your score was 5000");
+    }
+}
