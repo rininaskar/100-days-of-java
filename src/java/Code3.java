5807247b6f65ccfcca409194247f7b4976b5b3d1
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 13:02:12 +0530

    Expression Check

---
+++ src/java/Code3.java
@@ -0,0 +1,14 @@
+public class Code3 {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " + " another " + " still more");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+    }
+}
