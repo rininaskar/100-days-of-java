626a173fb967cdd6daf3f81246a44deb8f105281
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 12:53:17 +0530

    Expression 4

---
+++ src/java/Code.java
@@ -0,0 +1,10 @@
+public class Code {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+    }
+}
