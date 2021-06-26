4c140b47fd83eb9bc733bd6945122b41f1bea30d
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 12:44:32 +0530

    Expression 2

---
+++ src/java/Expression5.java
@@ -0,0 +1,10 @@
+public class Expression5 {
+
+    public static void main(String[] args) {
+
+        int score = 100;
+        if (score > 90) {
+            System.out.println("You got the high score");
+        }
+    }
+}
