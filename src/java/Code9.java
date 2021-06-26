71ec5c7043b02ab6a549d4e95831fc2061344270
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 13:19:57 +0530

    Expressions Checking

---
+++ src/java/Code9.java
@@ -0,0 +1,15 @@
+public class Code9 {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 4000) {
+            System.out.println("Your score was 5000");
+            System.out.println("This was executed");
+        }
+    }
+}
