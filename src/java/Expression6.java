30a28cb41f5687e2957cf3ddcf4112c83ac640c8
Rini Naskar <rininaskar1999@gmail.com>
2021-06-26 12:46:05 +0530

    Expression 3

---
+++ src/java/Expression6.java
@@ -0,0 +1,11 @@
+public class Expression6 {
+
+    public static void main(String[] args) {
+
+        int score = 100;
+        if (score > 90) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+    }
+}
