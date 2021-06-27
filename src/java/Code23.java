097ed32494e2874da9c55cd70df31a706a722a72
Rini Naskar <rininaskar1999@gmail.com>
2021-06-27 17:57:51 +0530

    Challenge

--- src/java/Code23.java
+++ src/java/Code23.java
@@ -1,10 +1,12 @@
 public class Code23<gameOver> {
 
+    public static void main(String[] args) {
 
-    boolean gameOver = true;
-    int score = 5000;
-    int levelCompleted = 5;
-    int bonus = 100;
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
 
 
 //        if (score < 5000 &&  score >1000) {
@@ -14,8 +16,18 @@ public class Code23<gameOver> {
 //        } else {
 //            System.out.println("Got here");
 //        }
+        //Challenge
+        boolean newgameOver = true;
+        int newscore = 10000;
+        int newlevelCompleted = 8;
+        int newbonus = 200;
+
+        if(newgameOver ) {
+            int finalScore = newscore + (newlevelCompleted * newbonus);
+            System.out.println("Your final score was " + finalScore);
+        }
 
 
-
+    }
 
 }
