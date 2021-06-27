483ef1754f9f6722ce15e33d60363496b40f002e
Rini Naskar <rininaskar1999@gmail.com>
2021-06-27 18:00:57 +0530

    Challenge 2

---
+++ src/java/Code24.java
@@ -0,0 +1,42 @@
+public class Code24 {
+
+    public static void main(String[] args) {
+
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+//        if (score < 5000 &&  score >1000) {
+//            System.out.println("Your score was  less than 5000 but greater than 1000");
+//        } else if(score < 1000) {
+//            System.out.println("Your score was less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+        //Challenge
+//        boolean newgameOver = true;
+//        int newscore = 10000;
+//        int newlevelCompleted = 8;
+//        int newbonus = 200;
+//
+//        if(newgameOver ) {
+//            int finalScore = newscore + (newlevelCompleted * newbonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
+
+
+        score = 10000;
+         levelCompleted = 8;
+         bonus = 200;
+
+        if(gameOver ) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
+    }
+}
