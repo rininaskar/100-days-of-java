453f9236c6011b2437c199a528d6eef4064f489f
Rini Naskar <rininaskar1999@gmail.com>
2021-06-27 16:33:15 +0530

    Expressions Check < 5000

---
+++ src/java/Code15.java
@@ -0,0 +1,18 @@
+public class Code15 {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        if (score < 5000) {
+            System.out.println("Your score was less than 5000");
+    }
+
+
+
+    }
+}
