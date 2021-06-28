3e3df1e99ac62fc631ee294b92327728d1018b2e
Rini Naskar <rininaskar1999@gmail.com>
2021-06-28 19:53:21 +0530

    Recap

--- src/java/Example3.java
+++ src/java/Example3.java
@@ -4,7 +4,7 @@ public class Example3 {
 
         int score = 200;
 
-        if (score >= 5000) {
+        if (score >= 3000) {
             System.out.println("Your score was >= to 5000 ");
         } else if (score < 1000 && score >= 500) {
             System.out.println("Your score was < 1000 but >= 500");
