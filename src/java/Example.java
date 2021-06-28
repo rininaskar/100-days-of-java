d320ffd2703e6d98f1c3c1dfdbabffebb20db746
Rini Naskar <rininaskar1999@gmail.com>
2021-06-28 19:06:14 +0530

    if else

---
+++ src/java/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+
+    public static void main(String[] args) {
+
+        int score = 200;
+
+        if (score >= 5000) {
+            System.out.println("Your score was >= to 5000 ");
+        }
+    }
+}
