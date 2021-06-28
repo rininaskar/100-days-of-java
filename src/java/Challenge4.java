baa12b8d2461480bcb8585ade8196046ec014a6a
Rini Naskar <rininaskar1999@gmail.com>
2021-06-28 20:01:45 +0530

    Challenge

--- src/java/Challenge4.java
+++ src/java/Challenge4.java
@@ -4,7 +4,7 @@ public class Challenge4 {
 
         double myFirstValue = 20.00d;
         double mySecondValue = 80.00d;
-        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        double myValuesTotal =  myFirstValue + mySecondValue * 100.00d;
         System.out.println("MyValuesTotal = " + myValuesTotal);
         double theRemainder = myValuesTotal % 40.00d;
         System.out.println("theRemainder = " + theRemainder);
