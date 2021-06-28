7cbea0bb68cb2a7e3d02706150ce7edb994d799c
Rini Naskar <rininaskar1999@gmail.com>
2021-06-28 19:11:18 +0530

    if else Recap else

---
+++ src/java/Example3.java
@@ -0,0 +1,15 @@
+public class Example3 {
+
+    public static void main(String[] args) {
+
+        int score = 200;
+
+        if (score >= 5000) {
+            System.out.println("Your score was >= to 5000 ");
+        } else if (score < 1000 && score >= 500) {
+            System.out.println("Your score was < 1000 but >= 500");
+        } else {
+            System.out.println("Your score was < 500");
+        }
+    }
+}
