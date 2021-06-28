e62bf068868353728655710ccf6bf6baca9683d4
Rini Naskar <rininaskar1999@gmail.com>
2021-06-28 19:52:43 +0530

    Recap

--- src/java/Example.java
+++ src/java/Example.java
@@ -2,7 +2,7 @@ public class Example {
 
     public static void main(String[] args) {
 
-        int score = 200;
+        int score =  200;
 
         if (score >= 5000) {
             System.out.println("Your score was >= to 5000 ");
