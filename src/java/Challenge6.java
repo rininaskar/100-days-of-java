726bd78f32a7f847b242dea53cfbd06ef5aaceb7
Rini Naskar <rininaskar1999@gmail.com>
2021-07-03 19:21:08 +0530

    Challenge

--- src/java/Challenge6.java
+++ src/java/Challenge6.java
@@ -28,7 +28,7 @@ public class Challenge6 {
             return 1;
         } else if (playerScore > 500 && playerScore < 1000) {
             return 2;
-        } else if(playerScore > 200 && playerScore < 500) {
+        } else if(playerScore > 100 && playerScore < 500) {
             return  3;
         } else {
             return 4;
