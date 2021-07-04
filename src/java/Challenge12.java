956011086d05c650ef696d24ba0b00320b5a10df
Rini Naskar <rininaskar1999@gmail.com>
2021-07-04 17:41:52 +0530

    Challenge

--- src/java/Challenge12.java
+++ src/java/Challenge12.java
@@ -48,7 +48,7 @@ public class Challenge12 {
         if(playerScore > 1000) {
             return 1;
         } else if (playerScore > 500 && playerScore < 1000) {
-            return 2;
+            return 8;
         } else if(playerScore > 100 && playerScore < 500) {
             return  3;
         } else {
