42d5f43a5f11e6f7525e372e2934baf90485fd75
Rini Naskar <rininaskar1999@gmail.com>
2021-07-05 20:29:10 +0530

    Challenge

--- src/java/Challenge12.java
+++ src/java/Challenge12.java
@@ -47,7 +47,7 @@ public class Challenge12 {
     public static int calculateHighScorePosition(int  playerScore) {
         if(playerScore > 1000) {
             return 1;
-        } else if (playerScore > 500 && playerScore <  1000) {
+        } else if (playerScore > 500 && playerScore < 1000) {
             return 2;
         } else if(playerScore > 100 && playerScore < 500) {
             return  3;
