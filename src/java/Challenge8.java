710fc108cd3f9288b0e7fe6955ec2b0523c0df12
Rini Naskar <rininaskar1999@gmail.com>
2021-07-07 15:59:18 +0530

    Challenge

--- src/java/Challenge8.java
+++ src/java/Challenge8.java
@@ -4,7 +4,7 @@ public class Challenge8 {
 
         boolean gameOver = true;
         int score = 800;
-        int levelCompleted = 5;
+        int levelCompleted = 50;
         int bonus = 100;
 
         int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
