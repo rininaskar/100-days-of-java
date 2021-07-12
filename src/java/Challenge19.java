5f203447223103415bd571cf9f30c3a578d9d592
Rini Naskar <rininaskar1999@gmail.com>
2021-07-12 18:38:02 +0530

    Challenge

--- src/java/Challenge19.java
+++ src/java/Challenge19.java
@@ -3,7 +3,7 @@ public class Challenge19 {
 
         boolean gameOver = true;
         int score = 800;
-        int levelCompleted = 6;
+        int levelCompleted = 7;
         int bonus = 100;
 
         int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
