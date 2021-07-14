1dc196ffc4e9a1b4bebd13ac46cebbf7ec67d06c
Rini Naskar <rininaskar1999@gmail.com>
2021-07-14 15:20:05 +0530

    Challenge

--- src/java/Challenge19.java
+++ src/java/Challenge19.java
@@ -17,7 +17,7 @@ public class Challenge19 {
         System.out.println("Your final score was " + highScore);
 
         int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScorePosition("TimMY", highScorePosition);
+        displayHighScorePosition("TimM", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(900);
         displayHighScorePosition("Bob", highScorePosition);
