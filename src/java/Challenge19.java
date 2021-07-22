567814d9a3f64a5761656855c7e9c07b5f33575d
Rini Naskar <rininaskar1999@gmail.com>
2021-07-22 19:10:53 +0530

    Challenge

--- src/java/Challenge19.java
+++ src/java/Challenge19.java
@@ -14,7 +14,7 @@ public class Challenge19 {
         bonus = 200;
 
         highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " +highScore);
+        System.out.println("Your final score was " + highScore);
 
         int highScorePosition = calculateHighScorePosition(1500);
         displayHighScorePosition("Tim", highScorePosition);
