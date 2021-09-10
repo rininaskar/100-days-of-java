07d07e34d91927bcebc8a657cae88635b5f747bc
Rini Naskar <rininaskar1999@gmail.com>
2021-09-10 20:00:04 +0530

    Challenge

--- src/java/Challenge19.java
+++ src/java/Challenge19.java
@@ -17,7 +17,7 @@ public class Challenge19 {
         System.out.println("Your final score was " +   highScore);
 
         int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScorePosition("Tim", highScorePosition);
+        displayHighScorePosition("Timm", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(900);
         displayHighScorePosition("Bob", highScorePosition);
@@ -35,7 +35,7 @@ public class Challenge19 {
         displayHighScorePosition("Carol", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(100);
-        displayHighScorePosition("Franky", highScorePosition);
+        displayHighScorePosition("Frank", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
