4db683e2baf4196d2304b2bf547dbce33cbfa291
Rini Naskar <rininaskar1999@gmail.com>
2021-09-15 19:45:03 +0530

    Challenge

--- src/java/Challenge19.java
+++ src/java/Challenge19.java
@@ -23,7 +23,7 @@ public class Challenge19 {
         displayHighScorePosition("Bob", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(400);
-        displayHighScorePosition("Pery", highScorePosition);
+        displayHighScorePosition("Perry", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(500);
         displayHighScorePosition("Gilbert", highScorePosition);
