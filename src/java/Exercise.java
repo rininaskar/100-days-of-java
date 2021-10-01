3675e013d18a63cb6a8d550450755336a45bc941
Rini Naskar <rininaskar1999@gmail.com>
2021-10-01 21:12:58 +0530

    Coding

---
+++ src/java/Exercise.java
@@ -0,0 +1,11 @@
+public class Exercise {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+    }
+    public static void checkNumber(int number) {
+        if(number > 0 ) {
+            System.out.println("positive");
+        }
+    }
+}
