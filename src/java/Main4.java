0177f73d1c5d9a028ae5f855074e28af94c31f10
Rini Naskar <rininaskar1999@gmail.com>
2021-10-01 22:41:56 +0530

    Method Overloading

---
+++ src/java/Main4.java
@@ -0,0 +1,24 @@
+public class Main4 {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points ");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points ");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name ,No player score ");
+        return 0;
+    }
+}
