64dd8aa5b1fb3f20abed563ec7f1eebaec05777f
Rini Naskar <rininaskar1999@gmail.com>
2021-10-02 20:16:51 +0530

    Method Overloading

--- src/java/Main5.java
+++ src/java/Main5.java
@@ -11,6 +11,7 @@ public class Main5 {
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
 
         if((feet < 0) || ((inches < 0)  && (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
             return -1;
         }
         double centimeters = (feet * 12) * 2.54;
