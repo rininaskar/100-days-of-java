ec9d3d1b2ea9dbbde71d3a1c548ff1624d022f0c
Rini Naskar <rininaskar1999@gmail.com>
2021-10-02 20:59:36 +0530

    Method Overloading

---
+++ src/java/Main8.java
@@ -0,0 +1,61 @@
+public class Main8 {
+
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+        double centimeters = calcFeetAndInchesToCentimeters(-8, 0);
+        if(centimeters <0.0) {
+            System.out.println("Invalid parameters");
+        }
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        if((feet < 0) || ((inches < 0)  && (inches > 12))) {
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + "feet," + inches + "inches = " + centimeters + "cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+
+        if(inches < 0) {
+            return -1;
+        }
+
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + "inches is equal to" + feet + "feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, inches);
+    }
+
+
+
+
+
+
+
+
+
+
+
+        public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points ");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points ");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name ,No player score ");
+        return 0;
+    }
+}
