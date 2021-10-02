0be31767b30bfe54d80eb7543872b097f2454755
Rini Naskar <rininaskar1999@gmail.com>
2021-10-02 20:05:07 +0530

    Method Overloading

--- src/java/Main4.java
+++ src/java/Main4.java
@@ -5,7 +5,28 @@ public class Main4 {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+
     }
+
+    public static double calcFeetInchesToCentimeters(double feet, double inches) {
+
+        if((feet < 0) || ((inches < 0)  && (inches > 12))) {
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + "feet," + inches + "inches = " + centimeters + "cm");
+        return centimeters;
+    }
+
+
+
+
+
+
+
+
+
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored " + score + " points ");
