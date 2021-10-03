70c94badacc7b15f4f06f13d0217d766bb4d5c14
Rini Naskar <rininaskar1999@gmail.com>
2021-10-03 20:23:18 +0530

    Challenge Method Overloading

--- src/java/Challenge21.java
+++ src/java/Challenge21.java
@@ -2,7 +2,7 @@ public class Challenge21 {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(3945L));
     }
 
     private static String getDurationString(long minutes, long seconds) {
