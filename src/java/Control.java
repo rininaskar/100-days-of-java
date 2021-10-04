33010b6cb90d43af5f6d7256f512bdf249610bed
Rini Naskar <rininaskar1999@gmail.com>
2021-10-04 20:23:52 +0530

    Control Flow Statement

--- src/java/Control.java
+++ src/java/Control.java
@@ -1,7 +1,7 @@
 public class Control {
 
     public static void main(String[] args) {
-        int value = 2;
+        int value = 3;
         if(value == 1) {
             System.out.println("Value was 1");
         } else if(value == 2) {
