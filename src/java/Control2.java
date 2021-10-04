be9eb7ef0de6626866edffac1fa1dd75b1f42620
Rini Naskar <rininaskar1999@gmail.com>
2021-10-04 20:34:46 +0530

    Control Flow Statement

--- src/java/Control2.java
+++ src/java/Control2.java
@@ -23,6 +23,7 @@ public class Control2 {
 
             case 3: case 4: case 5:
                 System.out.println("Was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
 
             default:
                 System.out.println("Was not 1 or 2");
