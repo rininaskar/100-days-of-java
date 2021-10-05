1d8e7b54fe6f4b78b85bf68fd26389090359aa1d
Rini Naskar <rininaskar1999@gmail.com>
2021-10-05 19:04:59 +0530

    Control Flow Statement

---
+++ src/java/Control3.java
@@ -0,0 +1,22 @@
+public class Control3 {
+
+    public static void main(String[] args) {
+
+        char charValue = 'A';
+        switch (charValue) {
+            case 'A' :
+                System.out.println("A was found");
+                break;
+            case 'B' :
+                System.out.println("B was found");
+                break;
+            case 'C' : case 'D' : case 'E':
+                System.out.println(charValue + "was found");
+                break;
+
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break;
+        }
+    }
+}
