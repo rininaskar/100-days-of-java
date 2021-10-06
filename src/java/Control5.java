e9e9824b4fbd2447691c2f21de441f0da5fbf7c2
Rini Naskar <rininaskar1999@gmail.com>
2021-10-06 19:30:53 +0530

    Control Flow Statement

---
+++ src/java/Control5.java
@@ -0,0 +1,34 @@
+public class Control5 {
+
+    public static void main(String[] args) {
+
+        char charValue = 'G';
+        switch (charValue) {
+            case 'A' :
+                System.out.println("A was found");
+                break;
+            case 'B' :
+                System.out.println("B was found");
+                break;
+            case 'C' : case 'D' : case 'E':
+                System.out.println(charValue + " was found");
+                break;
+
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break;
+        }
+
+        String month = "JuNE";
+        switch(month.toLowerCase()) {
+            case "January":
+                System.out.println("Jan");
+                break;
+            case "June":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+    }
+}
