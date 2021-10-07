5db8875e11bbce3c6c473b6daefd720a4dbe5c83
Rini Naskar <rininaskar1999@gmail.com>
2021-10-07 22:06:22 +0530

    Control Flow Statement

---
+++ src/java/Challenge24.java
@@ -0,0 +1,39 @@
+public class Challenge24 {
+
+    public static void main(String[] args) {
+
+        printDayOfTheWeek(-1);
+
+    }
+
+     private static void printDayOfTheWeek(int day) {
+
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesay");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Day");
+                break;
+
+        }
+     }
+}
