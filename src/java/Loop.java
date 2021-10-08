ad5e89c05eeca784c69eaae18c31c2023f7c68dc
Rini Naskar <rininaskar1999@gmail.com>
2021-10-08 21:23:03 +0530

    For Statement

--- src/java/Loop.java
+++ src/java/Loop.java
@@ -3,7 +3,7 @@ public class Loop {
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0,2.0));
 
-    for (int i=1; i<5; i++) {
+    for (int i=1; i<6; i++) {
         System.out.println("Loop" + i + " Hello!");
     }
     }
