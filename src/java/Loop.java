77935c6c8ca6ff5806a633bbbdd834e0f7f5aa2b
Rini Naskar <rininaskar1999@gmail.com>
2021-10-08 21:08:11 +0530

    Control Flow Statement

---
+++ src/java/Loop.java
@@ -0,0 +1,10 @@
+public class Loop {
+
+    public static void main(String[] args) {
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0,2.0));
+    }
+
+    public static double calculateInterest(double amount,double interestRate) {
+        return (amount *(interestRate/100));
+    }
+}
