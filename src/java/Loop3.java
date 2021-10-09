4765aaa19fc79617574bbdbcffe5f5b33bcddea4
Rini Naskar <rininaskar1999@gmail.com>
2021-10-09 23:07:16 +0530

    For Statement

--- src/java/Loop3.java
+++ src/java/Loop3.java
@@ -11,6 +11,8 @@ public class Loop3 {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
         }
 
+        System.out.println("********************");
+
         for(int i=8; i>=2; i--) {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000.0, i)));
         }
