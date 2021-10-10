98cc7766c0fb43d365e1da61e72e802bac91b5a1
Rini Naskar <rininaskar1999@gmail.com>
2021-10-10 20:39:45 +0530

    Challenge 3 And 5 Statement

---
+++ src/java/Challenge25.java
@@ -0,0 +1,19 @@
+public class Challenge25 {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        int sum = 0;
+
+        for(int i=1; i <= 1000; i++)
+        {
+            if ((i % 3 == 0) && (i % 5 ==0)) {
+                count++;
+                sum += i;
+                System.out.println("Found Number = " + i);
+            }
+
+
+        }
+    }
+}
