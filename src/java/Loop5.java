316afc77920a7ebd1e3e9543c18969841c77cea1
Rini Naskar <rininaskar1999@gmail.com>
2021-10-11 21:16:52 +0530

    While And Do-While Loop

---
+++ src/java/Loop5.java
@@ -0,0 +1,11 @@
+public class Loop5 {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        while (count != 5) {
+            System.out.println("Count value is" + count);
+            count++;
+        }
+    }
+}
