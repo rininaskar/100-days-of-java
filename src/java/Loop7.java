70c37d96743bc3d955c6a1418de4eac3b6d2c80e
Rini Naskar <rininaskar1999@gmail.com>
2021-10-12 18:51:56 +0530

    While And Do-While Loop

---
+++ src/java/Loop7.java
@@ -0,0 +1,20 @@
+public class Loop7 {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        count = 1;
+        while(true) {
+            if(count == 6) {
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+}
