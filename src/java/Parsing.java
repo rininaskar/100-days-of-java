b77f01a84196115b3823afa679842d69f8a9ed2c
Rini Naskar <rininaskar1999@gmail.com>
2021-10-18 20:32:05 +0530

    Digit Sum Challenge

---
+++ src/java/Parsing.java
@@ -0,0 +1,8 @@
+public class Parsing {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2018" ;
+        System.out.println("numberAsString " + numberAsString);
+    }
+}
