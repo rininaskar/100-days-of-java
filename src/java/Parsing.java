4af3d14ccbd4f2d53278343a3284498dcd25afa8
Rini Naskar <rininaskar1999@gmail.com>
2021-10-18 20:44:23 +0530

    Parsing Values From A String

--- src/java/Parsing.java
+++ src/java/Parsing.java
@@ -7,5 +7,11 @@ public class Parsing {
 
         int number = Integer.parseInt(numberAsString);
         System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
     }
 }
