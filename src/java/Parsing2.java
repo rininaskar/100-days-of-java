48b6a8034acbef313b98238bdb1e707761356978
Rini Naskar <rininaskar1999@gmail.com>
2021-10-20 21:08:38 +0530

    Parsing Values From A String

--- src/java/Parsing2.java
+++ src/java/Parsing2.java
@@ -2,7 +2,7 @@ public class Parsing2 {
 
     public static void main(String[] args) {
 
-        String numberAsString = "2019" ;
+        String numberAsString = "2019a" ;
         System.out.println("numberAsString " + numberAsString);
 
         double number = Double.parseDouble(numberAsString);
