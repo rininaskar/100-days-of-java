6101fab29fdfa93bc22a76c1e5aaeea6cc30d65e
Rini Naskar <rininaskar1999@gmail.com>
2021-10-25 20:39:20 +0530

    Parsing Values From A String

---
+++ src/java/Parsing4.java
@@ -0,0 +1,17 @@
+public class Parsing4 {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2019" ;
+        System.out.println("numberAsString " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+    }
+}
