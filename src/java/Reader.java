25be5a4b8da1b31af58f4cc988f43754364780aa
Rini Naskar <rininaskar1999@gmail.com>
2021-10-28 19:54:31 +0530

    Reading User Input

--- src/java/Reader.java
+++ src/java/Reader.java
@@ -4,6 +4,7 @@ public class Reader {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth");
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
