d0e8fc2cfbd95098b7a44c601ae7b73172ce423e
Rini Naskar <rininaskar1999@gmail.com>
2021-10-31 20:38:13 +0530

    Reading User Input

---
+++ src/java/Reader2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reader2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2019 - yearOfBirth;
+
+        System.out.println(" Your name is " + name + ", and you are " + age + " years old.");
+
+        scanner.close();
+    }
+}
