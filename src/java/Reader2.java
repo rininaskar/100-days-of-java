e4933e528d42a27b6c8f768a10a2c4e36b0fe2cf
Rini Naskar <rininaskar1999@gmail.com>
2021-11-01 20:12:19 +0530

    Reading User Input

--- src/java/Reader2.java
+++ src/java/Reader2.java
@@ -9,20 +9,18 @@ public class Reader2 {
         boolean hasNextInt = scanner.hasNextInt();
 
         if (hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2019 - yearOfBirth;
 
+            if (age >= 0 && age <= 100) {
+                System.out.println(" Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
         }
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2019 - yearOfBirth;
-
-        if (age >= 0 && age <= 100) {
-            System.out.println(" Your name is " + name + ", and you are " + age + " years old.");
-        } else {
-            System.out.println("Invalid year of birth");
-        }
-
         scanner.close();
     }
 }
