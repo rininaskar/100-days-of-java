d264c85c9781135e2afa40e1b8dbfeac36aba105
Rini Naskar <rininaskar1999@gmail.com>
2021-11-04 21:00:35 +0530

    Reading User Input

---
+++ src/java/Challenge27.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Challenge27 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+        }
+    }
+}
