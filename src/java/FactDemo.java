f5d6bd67e281c5363538a1b769d776dc38e7a3ab
Rini Naskar <rininaskar1999@gmail.com>
2021-11-12 20:10:10 +0530

    College Programs

---
+++ src/java/FactDemo.java
@@ -0,0 +1,9 @@
+public class FactDemo {
+
+    public static void main(String[] args) {
+
+        int n;
+        if(args.length == 0)
+            System.out.println("No command line argument");
+    }
+}
