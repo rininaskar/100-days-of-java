e92d0187db5b4ddb8ea72a133386e220fdce3118
Rini Naskar <rininaskar1999@gmail.com>
2021-11-13 19:27:22 +0530

    College Programs

--- src/java/Prime1.java
+++ src/java/Prime1.java
@@ -9,5 +9,18 @@ public class Prime1 {
         Scanner sc= new Scanner(System.in);
         m = sc.nextInt();
         System.out.println("Enter the upper limit");
+        n = sc.nextInt();
+        System.out.println("The prime numbers are");
+        for(int j=m; j<=n; j++)
+        {
+            f=0;
+            for(i=2; i<j-1; i--)
+            {
+                if (j%i==0)
+                    f=1;
+            }
+            if(f==0)
+                System.out.println(j);
+        }
     }
 }
