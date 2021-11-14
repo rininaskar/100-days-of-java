b5c789018eef11f6de04b23b222042fb72c7799e
Rini Naskar <rininaskar1999@gmail.com>
2021-11-14 19:56:51 +0530

    College Programs

---
+++ src/java/Prime2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Prime2 {
+
+    public static void main(String[] args) {
+
+     int n1,n2,i,j,count;
+        System.out.println("Enter the lower limit");
+        Scanner sc= new Scanner(System.in);
+
+    }
+}
