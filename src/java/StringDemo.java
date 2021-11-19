2ff6389e41ded73451899d6b51c600f787e1c60f
Rini Naskar <rininaskar1999@gmail.com>
2021-11-19 19:23:56 +0530

    College Programs

---
+++ src/java/StringDemo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        String str,str1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n-------------------------\n");
+    }
+}
