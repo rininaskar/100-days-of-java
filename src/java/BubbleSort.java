e55c4363af565edb72cc7bf04de1e2b9ba32696c
Rini Naskar <rininaskar1999@gmail.com>
2021-11-24 21:49:53 +0530

    College Programs

---
+++ src/java/BubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int a[],i,j,n;
+        try
+        {
+            System.out.println("Enter the no of elements:");
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextInt();
+            a = new int[6];
+            System.out.println("Enter the elements");
+        } catch ( ArrayIndexOutOfBoundsException e) {
+
+        }
+    }
+}
