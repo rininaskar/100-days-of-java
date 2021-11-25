fb7705cb649af5811d7981e7f98bc241653c0ded
Rini Naskar <rininaskar1999@gmail.com>
2021-11-25 18:59:08 +0530

    College Programs

--- src/java/BubbleSort.java
+++ src/java/BubbleSort.java
@@ -14,6 +14,16 @@ public class BubbleSort {
             System.out.println("Enter the elements");
             for(i=0;i<n; i++)
                 a[i] = sc.nextInt();
+
+            for (i=0;i<n;i++){
+                for (j=0;j<n-i-1;j++){
+                    if(a[j] > a[j+1]){
+                        int t = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = t;
+                    }
+                }
+            }
         } catch ( ArrayIndexOutOfBoundsException e) {
 
 
