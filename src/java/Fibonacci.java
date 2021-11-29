98120f721af00289f5150e1ec3b3ef44f6d5fd3d
Rini Naskar <rininaskar1999@gmail.com>
2021-11-29 19:01:22 +0530

    College Programs

---
+++ src/java/Fibonacci.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int fib1=0,fib2=1,temp = 0,num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Enter the limit");
+    }
+}
