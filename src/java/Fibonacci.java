12bcf620300572c32416da57313ee337be50e302
Rini Naskar <rininaskar1999@gmail.com>
2021-11-29 19:03:27 +0530

    College Programs

--- src/java/Fibonacci.java
+++ src/java/Fibonacci.java
@@ -7,5 +7,12 @@ public class Fibonacci {
         int fib1=0,fib2=1,temp = 0,num;
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Enter the limit");
+        num = sc.nextInt();
+        System.out.print("  "+fib1);
+        System.out.print("  "+fib2);
+        for (int i=2;i<num;i++)
+        {
+            temp = fib1 + fib2;
+        }
     }
 }
