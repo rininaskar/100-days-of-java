eeded1c1f1b88ec1f5aeeb5588a28fcc30ddeb77
Rini Naskar <rininaskar1999@gmail.com>
2021-12-05 19:59:41 +0530

    College Programs

---
+++ src/java/BasicVector.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class BasicVector {
+
+    public static void main(String[] args) {
+
+        Vector <String> vct = new Vector<String>();
+        vct.add("First");
+        vct.add("Second");
+        vct.add("Third");
+        vct.add("Fourth");
+    }
+}
