37045da6c3b1e06d6b6b2d1e8638b360c4b82e46
Rini Naskar <rininaskar1999@gmail.com>
2021-12-16 19:39:34 +0530

    College Programs

---
+++ src/java/Vowel_Consonant.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Vowel_Consonant {
+
+    public static void main(String[] args) {
+
+        char n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character:");
+    }
+}
