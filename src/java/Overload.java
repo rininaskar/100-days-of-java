9b188982bbce623ad77cffa181bb3accb83eec45
Rini Naskar <rininaskar1999@gmail.com>
2021-12-21 18:55:34 +0530

    College Programs

--- src/java/Overload.java
+++ src/java/Overload.java
@@ -12,9 +12,11 @@ public class Overload
         }
         public static void main (String[]args) {
         {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the radius of ");
-            double r = sc.nextDouble();
+            double r;
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.println("Enter the radius of ");
+                r = sc.nextDouble();
+            }
             System.out.println("Area of circle is:" +area(r));
         }
     }
