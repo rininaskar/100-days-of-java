6200f150843add2385cf44c3daae98d123c8ae37
Rini Naskar <rininaskar1999@gmail.com>
2022-01-01 19:13:01 +0530

    College Programs

--- src/java/Overload.java
+++ src/java/Overload.java
@@ -6,10 +6,7 @@ public class Overload
         {
             return (3.14 * r * r);
         }
-        static int area(int l, int b)
-        {
-            return (l*b);
-        }
+
 
 
         public static void main (String[]args) {
