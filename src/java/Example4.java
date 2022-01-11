4e47fcd7383d81e0753932410cdc0b33d3a63468
Rini Naskar <rininaskar1999@gmail.com>
2022-01-11 18:44:40 +0530

    Exercise

---
+++ src/java/Example4.java
@@ -0,0 +1,14 @@
+public class Example4 {
+        public static void main(String[] args) {
+
+            int score = 200;
+
+            if (score >= 3000) {
+                System.out.println("Your score was >= to 5000 ");
+            } else if (score < 1000 && score >= 500) {
+                System.out.println("Your score was < 1000 but >= 500");
+            } else {
+                System.out.println("Your score was < 500");
+            }
+        }
+}
