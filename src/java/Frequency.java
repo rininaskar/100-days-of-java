60a2edf501c17b235938eb6c4f97fb2914c42085
Rini Naskar <rininaskar1999@gmail.com>
2022-01-24 19:44:38 +0530

    Exercise

--- src/java/Frequency.java
+++ src/java/Frequency.java
@@ -27,6 +27,6 @@ public class Frequency {
             if (fr[i] != visited)
                 System.out.println("    " + arr[i] + "    |    " + fr[i]);
         }
-        System.out.println("----------------------------------------");
+        System.out.println("-----------------------------------------");
     }
     }
