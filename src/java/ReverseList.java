b4fea08daaa8cdf8987f4c1b658de92f212d97f1
Rini <rininaskar1999@gmail.com>
2022-03-28 19:27:53 +0530

    Exercise

--- src/java/ReverseList.java
+++ src/java/ReverseList.java
@@ -1,97 +1,78 @@
 public class ReverseList {
 
-    class Node{
+    public class Node{
         int data;
-        Node previous;
         Node next;
-
         public Node(int data) {
             this.data = data;
         }
     }
 
-    //Represent the head and tail of the doubly linked list
-    Node head, tail = null;
+    //Declaring head and tail pointer as null.
+    public Node head = null;
+    public Node tail = null;
 
-    //addNode() will add a node to the list
-    public void addNode(int data) {
-        //Create a new node
+    //This function will add the new node at the end of the list.
+    public void add(int data){
+        //Create new node
         Node newNode = new Node(data);
-
-        //If list is empty
+        //Checks if the list is empty.
         if(head == null) {
-            //Both head and tail will point to newNode
-            head = tail = newNode;
-            //head's previous will point to null
-            head.previous = null;
-            //tail's next will point to null, as it is the last node of the list
-            tail.next = null;
+            //If list is empty, both head and tail would point to new node.
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
         }
         else {
-            //newNode will be added after tail such that tail's next will point to newNode
+            //tail will point to new node.
             tail.next = newNode;
-            //newNode's previous will point to tail
-            newNode.previous = tail;
-            //newNode will become new tail
+            //New node will become new tail.
             tail = newNode;
-            //As it is last node, tail's next will point to null
-            tail.next = null;
+            //Since, it is circular linked list tail will point to head.
+            tail.next = head;
         }
     }
 
-    //reverse() will reverse the doubly linked list
-    public void reverse() {
-        //Node current will point to head
-        Node current = head, temp = null;
-
-        //Swap the previous and next pointers of each node to reverse the direction of the list
-        while(current != null) {
-            temp = current.next;
-            current.next = current.previous;
-            current.previous = temp;
-            current = current.previous;
-        }
-        //Swap the head and tail pointers.
-        temp = head;
-        head = tail;
-        tail = temp;
-    }
-
-    //display() will print out the elements of the list
+    //Displays all the nodes in the list
     public void display() {
-        //Node current will point to head
         Node current = head;
         if(head == null) {
             System.out.println("List is empty");
-            return;
         }
-
-        while(current != null) {
-            //Prints each node by incrementing the pointer.
-
-            System.out.print(current.data + " ");
-            current = current.next;
+        else {
+            do{
+                //Prints each node by incrementing pointer.
+                System.out.print(" "+ current.data);
+                current = current.next;
+            }while(current != head);
+            System.out.println();
         }
     }
 
+    //Reverse the order of the nodes present in the list.
+    public void reverse(Node current) {
+        //Checks if the next node is head, if yes then prints it.
+        if(current.next == head) {
+            System.out.print(" "+current.data);
+            return;
+        }
+        //Recursively calls the reverse function
+        reverse(current.next);
+        System.out.print(" "+current.data);
+    }
+
     public static void main(String[] args) {
-
-        ReverseList dList = new ReverseList();
-        //Add nodes to the list
-        dList.addNode(1);
-        dList.addNode(2);
-        dList.addNode(3);
-        dList.addNode(4);
-        dList.addNode(5);
-
+        ReverseList cl = new ReverseList();
+        cl.add(1);
+        cl.add(2);
+        cl.add(3);
+        cl.add(4);
+        cl.add(5);
+        cl.add(6);
         System.out.println("Original List: ");
-        dList.display();
-
-        //Reverse the given list
-        dList.reverse();
-
-        //Displays the reversed list
-        System.out.println("\nReversed List: ");
-        dList.display();
+        cl.display();
+        System.out.println("Reversed List: ");
+        //Print reversed list
+        cl.reverse(cl.head);
     }
 }
