0c38f24c11425c455b42c2253c0af1ae24a27820
Rini <rininaskar1999@gmail.com>
2022-04-03 18:52:56 +0530

    Exercise

--- src/java/RemoveDuplicate.java
+++ src/java/RemoveDuplicate.java
@@ -2,70 +2,71 @@ public class RemoveDuplicate {
 
     class Node{
         int data;
-        Node previous;
         Node next;
+
         public Node(int data) {
             this.data = data;
+            this.next = null;
         }
     }
-    //Represent the head and tail of the doubly linked list
-    Node head, tail = null;
 
-    //addNode() will add a node to the list
+    //Represent the head and tail of the singly linked list
+    public Node head = null;
+    public Node tail = null;
+
+    //addNode() will add a new node to the list
     public void addNode(int data) {
         //Create a new node
         Node newNode = new Node(data);
 
-        //If list is empty
+        //Checks if the list is empty
         if(head == null) {
-            //Both head and tail will point to newNode
-            head = tail = newNode;
-            //head's previous will point to null
-            head.previous = null;
-            //tail's next will point to null, as it is the last node of the list
-            tail.next = null;
+            //If list is empty, both head and tail will point to new node
+            head = newNode;
+            tail = newNode;
         }
         else {
             //newNode will be added after tail such that tail's next will point to newNode
             tail.next = newNode;
-            //newNode's previous will point to tail
-            newNode.previous = tail;
-            //newNode will become new tail
+            //newNode will become new tail of the list
             tail = newNode;
-            //As it is last node, tail's next will point to null
-            tail.next = null;
         }
     }
-    //removeDuplicateNode() will remove duplicate nodes from the list
-    public void removeDuplicateNode() {
-        //Node current will point to head
-        Node current, index, temp;
 
-        //Checks whether list is empty
+    //removeDuplicate() will remove duplicate nodes from the list
+    public void removeDuplicate() {
+        //Node current will point to head
+        Node current = head, index = null, temp = null;
+
         if(head == null) {
             return;
         }
         else {
-            //Initially, current will point to head node
-            for(current = head; current != null; current = current.next) {
-                //index will point to node next to current
-                for(index = current.next; index != null; index = index.next) {
+            while(current != null){
+                //Node temp will point to previous node to index.
+                temp = current;
+                //Index will point to node next to current
+                index = current.next;
+
+                while(index != null) {
+                    //If current node's data is equal to index node's data
                     if(current.data == index.data) {
-                        //Store the duplicate node in temp
-                        temp = index;
-                        //index's previous node will point to node next to index thus, removes the duplicate node
-                        index.previous.next = index.next;
-                        if(index.next != null)
-                            index.next.previous = index.previous;
-                        //Delete duplicate node by making temp to null
-                        temp = null;
+                        //Here, index node is pointing to the node which is duplicate of current node
+                        //Skips the duplicate node by pointing to next node
+                        temp.next = index.next;
                     }
+                    else {
+                        //Temp will point to previous node of index.
+                        temp = index;
+                    }
+                    index = index.next;
                 }
+                current = current.next;
             }
         }
     }
 
-    //display() will print out the nodes of the list
+    //display() will display all the nodes present in the list
     public void display() {
         //Node current will point to head
         Node current = head;
@@ -74,8 +75,7 @@ public class RemoveDuplicate {
             return;
         }
         while(current != null) {
-            //Prints each node by incrementing the pointer.
-
+            //Prints each node by incrementing pointer
             System.out.print(current.data + " ");
             current = current.next;
         }
@@ -84,24 +84,24 @@ public class RemoveDuplicate {
 
     public static void main(String[] args) {
 
-        RemoveDuplicate dList = new RemoveDuplicate();
-        //Add nodes to the list
-        dList.addNode(1);
-        dList.addNode(2);
-        dList.addNode(3);
-        dList.addNode(2);
-        dList.addNode(2);
-        dList.addNode(4);
-        dList.addNode(5);
-        dList.addNode(9);
+        RemoveDuplicate sList = new RemoveDuplicate();
+
+        //Adds data to the list
+        sList.addNode(1);
+        sList.addNode(2);
+        sList.addNode(3);
+        sList.addNode(2);
+        sList.addNode(2);
+        sList.addNode(4);
+        sList.addNode(1);
 
         System.out.println("Originals list: ");
-        dList.display();
+        sList.display();
 
         //Removes duplicate nodes
-        dList.removeDuplicateNode();
+        sList.removeDuplicate();
 
         System.out.println("List after removing duplicates: ");
-        dList.display();
+        sList.display();
     }
 }
