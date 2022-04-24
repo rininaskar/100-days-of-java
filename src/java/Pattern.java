a56d1fbe0af3be63513b25df012f093e4bf34bfb
Rini <rininaskar1999@gmail.com>
2022-04-24 21:42:13 +0530

    Exercise

--- src/java/Pattern.java
+++ src/java/Pattern.java
@@ -1,14 +1,37 @@
 public class Pattern {
 
-    public static void main(String[] args){
-        int lines=10;
-        int i=1;
-        int j;
-        for(i=1;i<=lines;i++){// this loop is used to print the lines
-            for(j=1;j<=i;j++){// this loop is used to print lines
-                System.out.print(i*j+" ");
+    public static void main(String []args){
+
+        int lines=8;
+        int i,j;
+        for(i=1;i<lines;i++){// this loop is used to print the lines
+            for(j=1;j<=lines/2;j++){// this loop is used to print numbers
+                if(i==j){
+                    System.out.print(j);
+                }
+                else if(i>4 && j==lines-i){
+                    System.out.print(j);
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            j=j-2;
+            while(j>0){ //this loop is used to print numbers
+                if(i==j){
+                    System.out.print(j);
+                }
+                else if(i>4 && j==lines-i){
+                    System.out.print(j);
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+                j--;
             }
             System.out.println("");
         }
     }
+
 }
