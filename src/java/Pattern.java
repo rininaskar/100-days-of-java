bf81833c63999c9facc6d8386d22fd3ed7698c05
Rini <rininaskar1999@gmail.com>
2022-04-25 18:57:05 +0530

    Exercise

--- src/java/Pattern.java
+++ src/java/Pattern.java
@@ -1,37 +1,110 @@
 public class Pattern {
 
     public static void main(String []args){
+        int matrix[][]=new int[5][5];
+        int i,j,k,l;
+        int direction=1;
+        for(i=0;i<5;i++){
+            for(j=0;j<5;j++){
+                matrix[i][j]=0;
+            }
+        }
 
-        int lines=8;
-        int i,j;
-        for(i=1;i<lines;i++){// this loop is used to print the lines
-            for(j=1;j<=lines/2;j++){// this loop is used to print numbers
-                if(i==j){
-                    System.out.print(j);
+        for(i=1,j=0,k=0;i<=16;i++){
+            matrix[j][k]=i;
+
+            switch(direction){
+                case 1:if(k+1<5){
+                    if(matrix[j][k+1]==0){
+                        k++;
+                        continue;
+                    }
+                    else{
+
+                        j++;
+                        direction=2;
+                        continue;
+                    }
                 }
-                else if(i>4 && j==lines-i){
-                    System.out.print(j);
+
+                else{
+                    j++;
+                    direction=2;
+                    continue;
+                }
+
+
+
+
+                case 2:if(j+1<5){
+
+                    if(matrix[j+1][k]==0){
+                        j++;
+                        continue;
+                    }
+                    else{
+                        direction=3;
+                        k--;
+                        continue;
+                    }
                 }
                 else{
-                    System.out.print(" ");
+                    direction=3;
+                    k--;
+                    continue;
+                }
+
+
+
+
+                case 3:if(k-1>=0){
+                    if(matrix[j][k-1]==0){
+
+                        k--;
+                        continue;
+                    }
+                    else{
+                        direction=4;
+                        j--;
+                        continue;
+                    }
+                }
+                else{
+                    direction=4;
+                    j--;
+                    continue;
+                }
+
+
+                case 4:if(j-1>=0){
+                    if(matrix[j-1][k]==0){
+
+                        j--;
+                        continue;
+                    }
+                    else{
+                        k++;
+                        direction=1;
+                        continue;
+                    }
+                }
+                else{
+                    k++;
+                    direction=1;
+                    continue;
                 }
             }
-            j=j-2;
-            while(j>0){ //this loop is used to print numbers
-                if(i==j){
-                    System.out.print(j);
-                }
-                else if(i>4 && j==lines-i){
-                    System.out.print(j);
+        }
+        for(i=0;i<5;i++){
+            for(j=0;j<5;j++){
+                if(matrix[i][j]==0){
+                    System.out.print("\t");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print(matrix[i][j]+"\t");
                 }
-
-                j--;
             }
             System.out.println("");
         }
-    }
 
-}
+    }
