d9ef7d191d210fda2f388540c7b595e5f6f3c5d4
Rini <rininaskar1999@gmail.com>
2022-05-02 18:19:15 +0530

    Exercise

--- src/java/Pattern.java
+++ src/java/Pattern.java
@@ -1,16 +1,22 @@
 public class Pattern {
 
     public static void main(String[] args) {
-        int size;
-        size = 6;
-        for (int i = size; i != 0; i--) {
-            for (int j = 0; j < size - i; j++) {
-                System.out.print(" ");
+        int coe = 1, rows = 6;
+        for (int i = 0; i < rows; i++) {
+            for (int space = 1; space < rows - i; ++space) {
+                System.out.print("  ");
             }
-            for (int k = 0; k < (2 * i - 1); k++) {
-                System.out.print("*");
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || i == 0)
+                    coe = 1;
+                else
+                    coe = coe * (i - j + 1) / j;
+
+                System.out.printf("%4d", coe);
             }
             System.out.println();
         }
     }
 }
+
