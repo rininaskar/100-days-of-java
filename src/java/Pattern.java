53a87179517a68104729a96de3bbd7bfa8b26a46
Rini <rininaskar1999@gmail.com>
2022-05-05 20:26:18 +0530

    Exercise

--- src/java/Pattern.java
+++ src/java/Pattern.java
@@ -1,14 +1,21 @@
 public class Pattern {
 
-    public static void main(String[] args) {
-        int i, j;
-        int n = 6;
-        for (i = n; i > 0; i--) {
-            for (j = 0; j < i; j++) {
+    public static void main(String[] args)
+    {
+        int size = 0;
+        Character c;
+        System.out.println();
+        size = 5;
+        int i, j, k;
+        for (i = 0; i < size + 1; i++) { for (j = size; j > i; j--) {
+            System.out.print(" ");
+        }
+            for (k = 0; k < (2 * i - 1); k++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
+}
 
 
