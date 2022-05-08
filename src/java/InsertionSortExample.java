9f40aed685b63802b0e8890a4cfb098183cd548d
Rini <rininaskar1999@gmail.com>
2022-05-08 21:28:37 +0530

    Exercise

--- src/java/InsertionSortExample.java
+++ src/java/InsertionSortExample.java
@@ -3,12 +3,20 @@ public class InsertionSortExample {
         int n = array.length;
         for (int j = 1; j < n; j++) {
             int key = array[j];
-            int i = j-1;
-            while ( (i > -1) && ( array [i] > key ) ) {
-                array [i+1] = array [i];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
                 i--;
             }
-            array[i+2] = key;
+            array[i + 2] = key;
+        }
+    }
+
+    public static void main(String a[]) {
+        int[] arr1 = {9, 14, 3, 2, 43, 11, 58, 22};
+        System.out.println("Before Insertion Sort");
+        for (int i : arr1) {
+            System.out.print(i + " ");
         }
     }
 }
